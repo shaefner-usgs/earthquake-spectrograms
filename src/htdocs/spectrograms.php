dbdbe44aeeadd7b0547729d0ce8ce83c5862ea6a
Scott Haefner <shaefner@usgs.gov>
2017-03-14 16:16:40 -0700

    customize title for each seismogram(s) page

--- src/htdocs/spectrograms.php
+++ src/htdocs/spectrograms.php
@@ -44,51 +44,56 @@ if (!isset($TEMPLATE)) {
     return $img;
   }
 
+  $listHtml = '<ul class="stations no-style">';
+
+  if ($id) { // show plots for a given station
+    // Query db to get station details
+    $rsStation = $db->queryStation($id);
+
+    // If station found, set subtitle; otherwise, show error
+    $row = $rsStation->fetch(PDO::FETCH_ASSOC);
+    if ($row) {
+      $instrument = $row['site'] . ' ' . $row['type'] . ' ' . $row['network'] .
+        ' ' . $row['code'];
+      $subtitle = $instrument . ' (' . trim($row['name']) . ')';
+    } else {
+      print '<p class="alert info">Station not found</p>';
+      return;
+    }
+
+    // Loop thru past 15 days
+    for ($i = 0; $i < 15; $i ++) {
+      $date = date('Ymd', strtotime('-' . $i . ' day'));
+      $imgTitle = date('M j, Y', strtotime($date));
+      $img = getImg($date, $id, $instrument);
+
+      $listHtml .= "<li><h3>$imgTitle</h3>$img</li>";
+    }
+  } else { // show plots for all stations on a given date
+    $subtitle = 'All Stations';
+    $header = getHeaderComponents($date);
+
+    // Query db to get a list of stations
+    $rsStations = $db->queryStations();
+
+    while ($row = $rsStations->fetch(PDO::FETCH_ASSOC)) {
+      $instrument = $row['site'] . ' ' . $row['type'] . ' ' . $row['network'] .
+        ' ' . $row['code'];
+      $img = getImg($date, $row['id'], $instrument);
+
+      $listHtml .= "<li><h3>$instrument</h3>$img</li>";
+    }
+  }
+
+  $listHtml .= '</ul>';
+
+  $TITLETAG = "Spectrograms | $subtitle";
+  if ($header['title']) {
+    $TITLETAG .= ' - ' . $header['title'];
+  }
+
   include 'template.inc.php';
 }
-
-$listHtml = '<ul class="stations no-style">';
-
-if ($id) { // show plots for a given station
-  // Query db to get station details
-  $rsStation = $db->queryStation($id);
-
-  // If station found, set subtitle; otherwise, show error
-  $row = $rsStation->fetch(PDO::FETCH_ASSOC);
-  if ($row) {
-    $instrument = $row['site'] . ' ' . $row['type'] . ' ' . $row['network'] .
-      ' ' . $row['code'];
-    $subtitle = $instrument . ' (' . trim($row['name']) . ')';
-  } else {
-    print '<p class="alert info">Station not found</p>';
-    return;
-  }
-
-  // Loop thru past 15 days
-  for ($i = 0; $i < 15; $i ++) {
-    $date = date('Ymd', strtotime('-' . $i . ' day'));
-    $imgTitle = date('M j, Y', strtotime($date));
-    $img = getImg($date, $id, $instrument);
-
-    $listHtml .= "<li><h3>$imgTitle</h3>$img</li>";
-  }
-} else { // show plots for all stations on a given date
-  $subtitle = 'All Stations';
-  $header = getHeaderComponents($date);
-
-  // Query db to get a list of stations
-  $rsStations = $db->queryStations();
-
-  while ($row = $rsStations->fetch(PDO::FETCH_ASSOC)) {
-    $instrument = $row['site'] . ' ' . $row['type'] . ' ' . $row['network'] .
-      ' ' . $row['code'];
-    $img = getImg($date, $row['id'], $instrument);
-
-    $listHtml .= "<li><h3>$instrument</h3>$img</li>";
-  }
-}
-
-$listHtml .= '</ul>';
 
 ?>
 
