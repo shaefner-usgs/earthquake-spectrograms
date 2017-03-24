bb805d72e5477aefd42e6dea16919405ba556288
Scott Haefner <shaefner@usgs.gov>
2017-03-24 14:08:55 -0700

    change cache time to 10mins

--- src/lib/pre-install.php
+++ src/lib/pre-install.php
@@ -95,6 +95,6 @@ file_put_contents($HTTPD_CONF, '
     </LimitExcept>
 
     ExpiresActive on
-    ExpiresDefault "access plus 1 days"
+    ExpiresDefault "access plus 10 minutes"
   </Location>
 ');
